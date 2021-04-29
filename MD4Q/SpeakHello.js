8cbdb1d65386b9efe56c47a35a895e04d67225b4
Mehdi <80045406+mehdij87@users.noreply.github.com>
2021-04-29 10:57:48 +0300

    MD4 Hard finished

--- MD4Q/SpeakHello.js
+++ MD4Q/SpeakHello.js
@@ -1,11 +1,13 @@
-// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+(function(window) {
+  // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var helloSpeaker =
+  var helloSpeaker = {};
+
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
@@ -13,7 +15,7 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
+helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
@@ -21,4 +23,5 @@ function speak(name) {
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
+window.helloSpeaker = helloSpeaker;
+}) (window);
