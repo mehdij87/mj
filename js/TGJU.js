58ecf7b1d16d4004942945e8e57ebbf36cd222c1
Mehdi <80045406+mehdij87@users.noreply.github.com>
2021-12-23 10:48:52 +0300

    Disc+TGJU

---
+++ js/TGJU.js
@@ -0,0 +1,576 @@
+var tgju_data = {
+  ons: {
+    title: "\u0627\u0648\u0646\u0633 \u0637\u0644\u0627",
+    p: "1,806.63",
+    h: "1,808.26",
+    l: "1,803.20",
+    d: "2.91",
+    dp: 0.16,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f0",
+  },
+  mesghal: {
+    title: "\u0645\u062b\u0642\u0627\u0644 \u0637\u0644\u0627",
+    p: "56,670,000",
+    h: "56,800,000",
+    l: "56,630,000",
+    d: "290,000",
+    dp: 0.51,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f2",
+  },
+  geram18: {
+    title: "\u06af\u0631\u0645 \u0637\u0644\u0627\u06cc \u06f1\u06f8",
+    p: "13,095,000",
+    h: "13,109,000",
+    l: "13,074,000",
+    d: "90,000",
+    dp: 0.69,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  geram24: {
+    title: "\u06af\u0631\u0645 \u0637\u0644\u0627\u06cc \u06f2\u06f4",
+    p: "17,459,000",
+    h: "17,478,000",
+    l: "17,432,000",
+    d: "120,000",
+    dp: 0.69,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  silver: {
+    title: "\u0627\u0646\u0633 \u0646\u0642\u0631\u0647",
+    p: "22.88",
+    h: "22.90",
+    l: "22.76",
+    d: "0.07",
+    dp: 0.3,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f1:\u06f3\u06f1",
+  },
+  platinum: {
+    title: "\u0627\u0646\u0633 \u067e\u0644\u0627\u062a\u06cc\u0646",
+    p: "969.65",
+    h: "969.80",
+    l: "956.80",
+    d: "2.95",
+    dp: 0.31,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f0:\u06f4\u06f9",
+  },
+  palladium: {
+    title:
+      "\u0627\u0646\u0633 \u067e\u0627\u0644\u0627\u062f\u06cc\u0648\u0645",
+    p: "1,853.78",
+    h: "1,893.50",
+    l: "1,837.53",
+    d: "29.50",
+    dp: 1.59,
+    dt: "low",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f0",
+  },
+  oil: {
+    title: "\u0646\u0641\u062a \u0633\u0628\u06a9",
+    p: "72.8",
+    h: "73.19",
+    l: "72.67",
+    d: "0.02",
+    dp: 0.03,
+    dt: "low",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f0",
+  },
+  oil_brent: {
+    title: "\u0646\u0641\u062a \u0628\u0631\u0646\u062a",
+    p: "75.28",
+    h: "75.71",
+    l: "75.16",
+    d: "0.1",
+    dp: 0.13,
+    dt: "low",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f0",
+  },
+  oil_opec: {
+    title: "\u0646\u0641\u062a \u0627\u067e\u06a9",
+    p: "74.23",
+    h: "74.23",
+    l: "74.23",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f2\u06f9 \u0622\u0630\u0631",
+  },
+  sekeb: {
+    title:
+      "\u0633\u06a9\u0647 \u0628\u0647\u0627\u0631 \u0622\u0632\u0627\u062f\u06cc",
+    p: "128,990,000",
+    h: "129,010,000",
+    l: "128,950,000",
+    d: "980,000",
+    dp: 0.77,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  sekee: {
+    title: "\u0633\u06a9\u0647 \u0627\u0645\u0627\u0645\u06cc",
+    p: "132,410,000",
+    h: "132,560,000",
+    l: "132,350,000",
+    d: "300,000",
+    dp: 0.23,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f6",
+  },
+  nim: {
+    title: "\u0646\u06cc\u0645 \u0633\u06a9\u0647",
+    p: "68,500,000",
+    h: "68,500,000",
+    l: "68,200,000",
+    d: "500,000",
+    dp: 0.74,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f1:\u06f3\u06f3",
+  },
+  rob: {
+    title: "\u0631\u0628\u0639 \u0633\u06a9\u0647",
+    p: "38,300,000",
+    h: "38,300,000",
+    l: "38,120,000",
+    d: "300,000",
+    dp: 0.79,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f1:\u06f3\u06f3",
+  },
+  gerami: {
+    title: "\u0633\u06a9\u0647 \u06af\u0631\u0645\u06cc",
+    p: "23,000,000",
+    h: "23,070,000",
+    l: "23,000,000",
+    d: "50,000",
+    dp: 0.22,
+    dt: "low",
+    t: "\u06f1\u06f1:\u06f0\u06f1:\u06f3\u06f3",
+  },
+  bourse: {
+    title: "\u0634\u0627\u062e\u0635 \u0628\u0648\u0631\u0633",
+    p: "1,359,011.0",
+    h: "1,359,012.5",
+    l: "1,321,753.1",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1 \u062f\u06cc",
+  },
+  price_dollar_rl: {
+    title: "\u062f\u0644\u0627\u0631",
+    p: "274,020",
+    h: "274,020",
+    l: "273,860",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_eur: {
+    title: "\u06cc\u0648\u0631\u0648",
+    p: "310,380",
+    h: "310,710",
+    l: "310,380",
+    d: "150",
+    dp: 0.05,
+    dt: "low",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_aed: {
+    title: "\u062f\u0631\u0647\u0645 \u0627\u0645\u0627\u0631\u0627\u062a",
+    p: "82,580",
+    h: "82,780",
+    l: "82,550",
+    d: "150",
+    dp: 0.18,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_gbp: {
+    title: "\u067e\u0648\u0646\u062f",
+    p: "404,060",
+    h: "404,660",
+    l: "403,510",
+    d: "1,050",
+    dp: 0.26,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_sek: {
+    title: "\u06a9\u0631\u0648\u0646 \u0633\u0648\u0626\u062f",
+    p: "33,200",
+    h: "33,300",
+    l: "33,200",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1\u06f1:\u06f0\u06f8:\u06f2\u06f2",
+  },
+  price_dkk: {
+    title:
+      "\u06a9\u0631\u0648\u0646 \u062f\u0627\u0646\u0645\u0627\u0631\u06a9",
+    p: "45,900",
+    h: "46,000",
+    l: "45,900",
+    d: "100",
+    dp: 0.22,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f5:\u06f5\u06f9",
+  },
+  price_jpy: {
+    title: "\u06cc\u0646 \u0698\u0627\u067e\u0646",
+    p: "264,560",
+    h: "265,370",
+    l: "264,460",
+    d: "200",
+    dp: 0.08,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_myr: {
+    title:
+      "\u0631\u06cc\u0646\u06af\u06cc\u062a \u0645\u0627\u0644\u0632\u06cc",
+    p: "72,600",
+    h: "72,800",
+    l: "72,600",
+    d: "300",
+    dp: 0.41,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f6:\u06f2\u06f6",
+  },
+  price_kwd: {
+    title: "\u062f\u06cc\u0646\u0627\u0631 \u06a9\u0648\u06cc\u062a",
+    p: "1,001,100",
+    h: "1,003,500",
+    l: "1,000,700",
+    d: "2,400",
+    dp: 0.24,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_syp: {
+    title: "\u0644\u06cc\u0631 \u0633\u0648\u0631\u06cc\u0647",
+    p: "119",
+    h: "120",
+    l: "119",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1\u06f1:\u06f0\u06f8:\u06f0\u06f9",
+  },
+  price_rub: {
+    title: "\u0631\u0648\u0628\u0644 \u0631\u0648\u0633\u06cc\u0647",
+    p: "4,100",
+    h: "4,120",
+    l: "4,090",
+    d: "10",
+    dp: 0.24,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f2\u06f5",
+  },
+  price_omr: {
+    title: "\u0631\u06cc\u0627\u0644 \u0639\u0645\u0627\u0646",
+    p: "789,300",
+    h: "791,200",
+    l: "789,000",
+    d: "2,800",
+    dp: 0.36,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_sgd: {
+    title:
+      "\u062f\u0644\u0627\u0631 \u0633\u0646\u06af\u0627\u067e\u0648\u0631",
+    p: "222,700",
+    h: "223,200",
+    l: "222,500",
+    d: "700",
+    dp: 0.32,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f3\u06f6",
+  },
+  price_iqd: {
+    title: "\u062f\u06cc\u0646\u0627\u0631 \u0639\u0631\u0627\u0642",
+    p: "206",
+    h: "220",
+    l: "205",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1 \u062f\u06cc",
+  },
+  price_afn: {
+    title: "\u0627\u0641\u063a\u0627\u0646\u06cc",
+    p: "2,920",
+    h: "2,930",
+    l: "2,920",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1\u06f1:\u06f0\u06f4:\u06f2\u06f5",
+  },
+  price_azn: {
+    title:
+      "\u0645\u0646\u0627\u062a \u0622\u0630\u0631\u0628\u0627\u06cc\u062c\u0627\u0646",
+    p: "178,800",
+    h: "179,200",
+    l: "178,700",
+    d: "500",
+    dp: 0.28,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f3\u06f6",
+  },
+  price_aud: {
+    title:
+      "\u062f\u0644\u0627\u0631 \u0627\u0633\u062a\u0631\u0627\u0644\u06cc\u0627",
+    p: "220,200",
+    h: "220,600",
+    l: "220,000",
+    d: "800",
+    dp: 0.36,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f6",
+  },
+  price_cad: {
+    title: "\u062f\u0644\u0627\u0631 \u06a9\u0627\u0646\u0627\u062f\u0627",
+    p: "238,500",
+    h: "238,900",
+    l: "238,300",
+    d: "800",
+    dp: 0.34,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f6",
+  },
+  price_cny: {
+    title: "\u06cc\u0648\u0627\u0646 \u0686\u06cc\u0646",
+    p: "47,750",
+    h: "47,850",
+    l: "47,750",
+    d: "100",
+    dp: 0.21,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f4:\u06f2\u06f5",
+  },
+  price_chf: {
+    title: "\u0641\u0631\u0627\u0646\u06a9 \u0633\u0648\u0626\u06cc\u0633",
+    p: "329,600",
+    h: "330,600",
+    l: "329,500",
+    d: "700",
+    dp: 0.21,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f6",
+  },
+  price_nok: {
+    title: "\u06a9\u0631\u0648\u0646 \u0646\u0631\u0648\u0698",
+    p: "34,000",
+    h: "34,100",
+    l: "34,000",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f3\u06f6",
+  },
+  price_try: {
+    title: "\u0644\u06cc\u0631 \u062a\u0631\u06a9\u06cc\u0647",
+    p: "25,600",
+    h: "25,800",
+    l: "25,500",
+    d: "1,400",
+    dp: 5.79,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f9:\u06f3\u06f2",
+  },
+  price_hkd: {
+    title: "\u062f\u0644\u0627\u0631 \u0647\u0646\u06af \u06a9\u0646\u06af",
+    p: "39,400",
+    h: "39,500",
+    l: "39,400",
+    d: "100",
+    dp: 0.25,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f6:\u06f3\u06f2",
+  },
+  price_sar: {
+    title:
+      "\u0631\u06cc\u0627\u0644 \u0639\u0631\u0628\u0633\u062a\u0627\u0646",
+    p: "87,370",
+    h: "87,490",
+    l: "87,300",
+    d: "200",
+    dp: 0.23,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f4\u06f6",
+  },
+  price_inr: {
+    title: "\u0631\u0648\u067e\u06cc\u0647 \u0647\u0646\u062f",
+    p: "4,000",
+    h: "4,010",
+    l: "4,000",
+    d: "20",
+    dp: 0.5,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f6:\u06f2\u06f7",
+  },
+  price_pkr: {
+    title:
+      "\u0631\u0648\u067e\u06cc\u0647 \u067e\u0627\u06a9\u0633\u062a\u0627\u0646",
+    p: "1,739",
+    h: "1,743",
+    l: "1,738",
+    d: "3",
+    dp: 0.17,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f2\u06f5",
+  },
+  price_bhd: {
+    title: "\u062f\u06cc\u0646\u0627\u0631 \u0628\u062d\u0631\u06cc\u0646",
+    p: "802,400",
+    h: "804,300",
+    l: "802,100",
+    d: "1,600",
+    dp: 0.2,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_qar: {
+    title: "\u0631\u06cc\u0627\u0644 \u0642\u0637\u0631",
+    p: "84,000",
+    h: "84,200",
+    l: "84,000",
+    d: "100",
+    dp: 0.12,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f0\u06f8:\u06f0\u06f9",
+  },
+  price_nzd: {
+    title:
+      "\u062f\u0644\u0627\u0631 \u0646\u06cc\u0648\u0632\u06cc\u0644\u0646\u062f",
+    p: "208,500",
+    h: "208,800",
+    l: "208,300",
+    d: "700",
+    dp: 0.34,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f3\u06f6",
+  },
+  price_thb: {
+    title: "\u0628\u0627\u062a \u062a\u0627\u06cc\u0644\u0646\u062f",
+    p: "8,950",
+    h: "9,000",
+    l: "8,940",
+    d: "20",
+    dp: 0.22,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f5\u06f1",
+  },
+  price_amd: {
+    title:
+      "\u062f\u0631\u0627\u0645 \u0627\u0631\u0645\u0646\u0633\u062a\u0627\u0646",
+    p: "610",
+    h: "620",
+    l: "610",
+    d: "0",
+    dp: 0,
+    dt: "",
+    t: "\u06f1 \u062f\u06cc",
+  },
+  price_gel: {
+    title:
+      "\u0644\u0627\u0631\u06cc \u06af\u0631\u062c\u0633\u062a\u0627\u0646",
+    p: "98,300",
+    h: "98,500",
+    l: "98,200",
+    d: "200",
+    dp: 0.2,
+    dt: "high",
+    t: "\u06f1\u06f1:\u06f1\u06f2:\u06f2\u06f5",
+  },
+};
+if (tgju_data) {
+  var key, this_item, td;
+  var rows = "";
+
+  for (index in tgju_params.items) {
+    key = tgju_params.items[index];
+
+    this_item = tgju_data[key];
+    if (this_item === undefined) continue;
+
+    td = {
+      diff:
+        tgju_params.columns.indexOf("diff") >= 0
+          ? '<td class="nf">' +
+            '<span class="' +
+            this_item["dt"] +
+            '">(' +
+            this_item["dp"] +
+            "%) " +
+            this_item["d"] +
+            "</span>" +
+            "</td>"
+          : "",
+      low:
+        tgju_params.columns.indexOf("low") >= 0
+          ? '<td class="nf">' + this_item["l"] + "</td>"
+          : "",
+      high:
+        tgju_params.columns.indexOf("high") >= 0
+          ? '<td class="nf">' + this_item["h"] + "</td>"
+          : "",
+      time:
+        tgju_params.columns.indexOf("time") >= 0
+          ? '<td class="nf">' + this_item["t"] + "</td>"
+          : "",
+    };
+
+    rows +=
+      '<tr id="f-' +
+      key +
+      '"><th>' +
+      this_item["title"] +
+      '</th><td class="nf">' +
+      this_item["p"] +
+      "</td>" +
+      td.diff +
+      td.low +
+      td.high +
+      td.time +
+      "</tr>";
+  }
+
+  var th = {
+    diff: tgju_params.columns.indexOf("diff") >= 0 ? "<th>تغییر</th>" : "",
+    low: tgju_params.columns.indexOf("low") >= 0 ? "<th>کمترین</th>" : "",
+    high: tgju_params.columns.indexOf("high") >= 0 ? "<th>بیشترین</th>" : "",
+    time: tgju_params.columns.indexOf("time") >= 0 ? "<th>زمان</th>" : "",
+  };
+
+  var copyright = "";
+  var embed_style = "";
+  if (tgju_params.token === "webservice") {
+    copyright =
+      '<a href="http://www.tgju.org" target="_blank" class="tgju-copyright">WWW.TGJU.ORG</a>';
+    embed_style =
+      "#tgju-data-block #tgju table.data-table thead { 	display: none; }  #tgju-data-block #tgju table.data-table tbody tr { 	display: block !important; }  #tgju-data-block #tgju table.data-table tbody th { 	border-bottom: none !important; 	display: block !important; 	text-align: center !important; 	padding: 3px 10px 3px !important; 	background-color: #F1F1F1; }  #tgju-data-block #tgju table.data-table tbody td { 	display: block !important; 	text-align: center !important; 	width: 128px !important; 	padding: 3px 10px 5px !important; }  #tgju-data-title { 	text-align: center; 	color: #fff; 	font-family: tahoma; 	font-size: 12px; 	font-weight: bold; 	direction: rtl;  	background: #1A1A1A;     padding: 5px 5px 7px; }  .tgju-copyright { 	display: block; 	text-align: center; 	color: #eee !important; 	text-decoration: none; 	font-family: tahoma; 	font-size: 11px; 	margin-top: -15px; 	margin-bottom: 10px; 	direction: rtl;  	background: #1A1A1A;     padding: 4px 1px 6px;     margin-top: -20px; }  .tgju-copyright:hover, .tgju-copyright:focus{color:#eee !important}  .tgju-section-title { 	font: bold 12px/1 tahoma; 	text-align: center; 	background-color: #404042; 	padding: 7px 7px 8px; 	color: #FFF; }  #tgju table.data-table { 	padding: 0 !important; 	border-top: 0 !important; 	border-color: #1A1A1A !important; }  #tgju table.data-table tbody tr th, #tgju table.data-table tbody tr td { 	border-bottom: 1px solid #DCDCDC !important; }  body #tgju table.data-table tbody tr:hover { 	background: #DEDEDE !important; }  #tgju table.data-table thead th {     background: #1a1a1a;     color: #FFF;     text-shadow: none; }";
+  }
+
+  document.getElementById(tgju_params.placeholder).innerHTML =
+    "<style>#tgju table.data-table {width:100%;border-spacing:0;padding:1px;font:normal 11px tahoma;direction: rtl;}#tgju table.data-table {border:1px solid #e5e5e5;margin-bottom:20px;}table thead th{border-bottom:1px solid #e9e9e9;padding:6px 10px 9px;text-shadow:0 1px 1px #fff;background:#F7F7F7;text-align:right!important;}table tr td{padding:7px 0 10px;border-bottom:1px solid #efefef;}table tr:last-child td{border-bottom:0;}#tgju table.data-table tbody tr th{padding:6px 10px 8px;;border-bottom:1px solid #efefef;text-align:right!important;}#tgju table.data-table tbody tr th.tophead{background:#fff;}#tgju table.data-table tbody tr td{padding:6px 10px 8px;vertical-align:top;text-align:right;}#tgju table.data-table tbody tr:hover{background:#f1fdff!important;-webkit-transition:none;-moz-transition:none;-o-transition:none;-ms-transition:none;transition:none;}#tgju table.data-table tbody tr:hover th{-webkit-transition:none;-moz-transition:none;-o-transition:none;-ms-transition:none;transition:none;background-color:transparent!important;}#tgju table.data-table tbody tr:hover th.tophead{background:#fff!important;}#tgju table.data-table tbody tr:last-child td,#tgju table.data-table tbody tr:last-child th{border-bottom:none;}#tgju table.data-table.data-table-child{border:none!important;float:right;width:49%;-webkit-box-shadow:none!important;box-shadow:none!important;margin-bottom:0!important;}#tgju table.data-table.data-table-child-side{float:left;}#tgju .nf.high,#tgju .nf .high{color:#39bb88!important;}#tgju .nf.low,#tgju .nf .low{color:#ea2e2e!important;}" +
+    embed_style +
+    '</style><div id="tgju"><table class="data-table"><thead><tr><th>عنوان نرخ</th><th>قیمت زنده</th>' +
+    th.diff +
+    th.low +
+    th.high +
+    th.time +
+    "</tr></thead><tbody>" +
+    rows +
+    "</tbody></table>" +
+    copyright +
+    "</div>";
+}
